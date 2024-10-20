18bb6c50ed99e42e0ac8528f26b88440112e0f8c
Aadit Chadda <aaditkobe268@gmail.com>
2024-10-20 05:52:12 -0400

    github python scrapper (getting started without proper api codes)

---
+++ TikTokAPITesting/sidetest.py
@@ -0,0 +1,7 @@
+url = """
+https://www.tiktok.com/api/comment/list/?WebIdLastTime=1729409061&aid=1988&app_language=en&app_name=tiktok_web&aweme_id=7409522776685415685&browser_language=en-GB&browser_name=Mozilla&browser_online=true&browser_platform=MacIntel&browser_version=5.0%20%28Linux%3B%20Android%206.0%3B%20Nexus%205%20Build%2FMRA58N%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F128.0.0.0%20Mobile%20Safari%2F537.36&channel=tiktok_web&cookie_enabled=true&count=20&cursor=20&data_collection_enabled=true&device_id=7427755272205075973&device_platform=web_mobile&focus_state=false&from_page=video&history_len=3&is_fullscreen=true&is_page_visible=true&odinId=7427755310612186117&os=android&priority_region=&referer=&region=CA&screen_height=1146&screen_width=1534&tz_name=America%2FToronto&user_is_login=false&verifyFp=verify_latio6ct_5FzXpKng_5ZAZ_4unS_AkTf_iCW6vWcdOHIQ&webcast_language=en&msToken=4d0ijdu6VCFBvp_HohB2N62FDMl_N3tcQLdVx5Rfb7SPA-0jdsnLRsCZ-6VUBvwzWX4hQC1fuyyM6a9wf5h-IpFszFjJgSdW9oikd-mFdl0P8fAiUSN35Nljr_J_7hgFFbr7c6kj_zDsGbaMTIoLW6Y=&X-Bogus=DFSzswVuPKGANJoutQD4a2hyS0li&_signature=_02B4Z6wo00001bWehtgAAIDAhj7ndIGDRRW1voJAAAp036"""
+if "cursor" in url:
+    print(True)
+else:
+    print(False)
+
